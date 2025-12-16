4b1099f25f3c2cb0aaf3d644c2838509533af769
rao <1941731228@qq.com>
2025-12-16 16:05:56 +0800

    homePage-调整

---
+++ app/(user)/my/CoreFunction/index.tsx
@@ -0,0 +1,11 @@
+import { Section } from "@/app/(user)/style";
+import { FeatureChipOrange, FeatureChipPink, FeatureChipPurple } from "@/app/(user)/style";
+
+export default function CoreFeatures() {
+  return (
+    <Section className="mt-16">
+      <div className="flex gap-4 justify-center w-full">
+      </div>
+    </Section>
+  );
+}
