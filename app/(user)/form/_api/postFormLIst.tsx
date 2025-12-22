2351092c850b3bc3a6d2c229661cd88f0e1eab27
Rudi <1941731228@qq.com>
2025-12-22 21:06:09 +0800

    createPictureForm-初始化

---
+++ app/(user)/form/_api/postFormLIst.tsx
@@ -0,0 +1,10 @@
+// export interface PostFormLIst {
+//     child_age: string //"preschool",
+//     illustration_style: "cartoon",
+//     themes: [
+//       cognitive_learning
+//     ],
+//     story_overview:  "123",
+//     central_idea: "123"
+//   }
+
